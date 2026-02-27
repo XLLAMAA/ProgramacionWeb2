e9562fd1292c6b2da814235b9165aebf992723be
XLLAMAA <jaimeadanez11@gmail.com>
2026-02-27 16:04:07 +0100

    Feat: Middleware hecho

--- Ejercicios/T5/src/middleware/error.middleware.js
+++ Ejercicios/T5/src/middleware/error.middleware.js
@@ -1,4 +1,30 @@
-// Generic error middleware
-module.exports = (err, req, res, next) => {
-    res.status(err.status || 500).json({ error: err.message });
+// src/middleware/error.middleware.js
+export const errorMiddleware = (err, req, res, next) => {
+    console.error(err);
+
+    // Error Multer tamaño
+    if (err.code === "LIMIT_FILE_SIZE") {
+        return res.status(400).json({
+            error: "La imagen supera el tamaño máximo permitido (5MB)",
+        });
+    }
+
+    // Error tipo archivo
+    if (err.message?.includes("Solo se permiten imágenes")) {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+
+    // Error validación mongoose
+    if (err.name === "ValidationError") {
+        return res.status(400).json({
+            error: err.message,
+        });
+    }
+
+    // Error genérico
+    return res.status(500).json({
+        error: "Error interno del servidor",
+    });
 };
