6ec9c8ada227ebd3829a2b8253f9d3da8fadea7b
XLLAMAA <jaimeadanez11@gmail.com>
2026-02-27 14:09:15 +0100

    Feat: Las rutas hechas, separadas por funciones

--- Ejercicios/T5/src/routes/movies.routes.js
+++ Ejercicios/T5/src/routes/movies.routes.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const controller = require('../controllers/movies.controller');
-const { movieSchema } = require('../schemas/movie.schema');
-const validate = require('../middleware/validate.middleware');
+import { Router } from "express";
+import {
+    getMovies,
+    getByIdMovie,
+    createMovie,
+    updateMovie,
+    deleteMovieById,
+    rentMovieById,
+    returnMovieById,
+    setMovieCover,
+    getMovieCover,
+    getTopMovies,
+} from "../controllers/movies.controller.js";
 
-const router = express.Router();
+import { validate, validateObjectId } from '../middleware/validate.middleware.js';
+import { createMovieSchema, updateMovieSchema } from '../schemas/movie.schema.js';
 
-router.get('/', controller.getAll);
-router.post('/', validate(movieSchema), controller.create);
+const router = Router();
 
-module.exports = router;
+//Rutas GET
+router.get("/", getMovies)
+router.get("/:id", getByIdMovie);
+router.get("/:id/cover", getMovieCover);
+router.get("/stats/top", getTopMovies);
+
+//POST
+router.post("/", createMovie);
+router.post("/:id/rent", rentMovieById);
+router.post("/:id/return", returnMovieById);
+
+//PUT
+router.put("/:id", updateMovie);
+
+//DELETE
+router.delete("/:id", deleteMovieById);
+
+//PATCH
+router.patch("/:id/cover", setMovieCover);
+
+export default router;
