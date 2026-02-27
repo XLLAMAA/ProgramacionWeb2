1d175c6b9eb8d8e40f694d97dd74b9c05866eebe
XLLAMAA <jaimeadanez11@gmail.com>
2026-02-27 16:04:42 +0100

    Feat: Subida

--- Ejercicios/T5/src/app.js
+++ Ejercicios/T5/src/app.js
@@ -1,14 +1,19 @@
-// Entry point for the movies API
-const express = require('express');
-const routes = require('./routes');
+// src/app.js
+import express from "express";
+import routes from "./routes/index.js";
+import { errorMiddleware } from "./middleware/error.middleware.js";
 
 const app = express();
+
 app.use(express.json());
-app.use('/api', routes);
+app.use(express.urlencoded({ extended: true }));
 
-app.use((err, req, res, next) => {
-    // simple error handler
-    res.status(err.status || 500).json({ error: err.message });
-});
+// para acceder a carátulas también por /storage/<filename>
+app.use("/storage", express.static("src/storage"));
 
-module.exports = app;
+app.use("/api", routes);
+
+// siempre el último
+app.use(errorMiddleware);
+
+export default app;
