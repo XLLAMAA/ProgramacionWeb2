d434e4027831103fe951a3c7c4658db8882585e3
XLLAMAA <jaimeadanez11@gmail.com>
2026-03-20 19:54:21 +0100

    Feat: Autenticacion de JWT y Password

---
+++ Ejercicios/T8/src/utils/handlePassword.js
@@ -0,0 +1,26 @@
+import bcryptjs from 'bcryptjs';
+
+/**
+ * Cifrar una contrasenia
+ * @param{string} el string que le meto es la clearPass
+ * @returns {Promise<string>} - Hash de la contrasenia
+ */
+
+//FUNCION PARA ENCRIPTAR LA PASS
+export const encrypt = async (clearPassword) => {
+    const hash = await bcrypt.hash(clearPassword, 10);
+    return hash;
+}
+
+/**
+ * @param {string} clearPassword - Contraseña en texto plano
+ * @param {string} hashedPassword - Hash almacenado en BD
+ * @returns {Promise<boolean>} - true si coinciden
+ */
+
+//FUNCION PARA COMPARAR LA PASS
+export const compare = async (clearPassword, hashPassword) => {
+    const result = await bcryptjs.compare(clearPassword, hashPassword)
+    return result;
+}
+
