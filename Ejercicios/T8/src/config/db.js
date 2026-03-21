034a2a0980f07521ad81e0748af5d909b7cbafb2
XLLAMAA <jaimeadanez11@gmail.com>
2026-03-21 11:53:30 +0100

    Feat: Conecto La DB de Mongoo

--- Ejercicios/T8/src/config/db.js
+++ Ejercicios/T8/src/config/db.js
@@ -2,6 +2,28 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
 
+    try {
 
+        //Leo la url del archivo .env 
 
-}
+        const DB_URI = process.env.NODE_ENV === 'test'
+            ? process.env.MONGODB_TEST_URI
+            : process.env.MONGODB_URI;
+
+        //Configuracion de conexion 
+        await mongoose.connect(DB_URI)
+
+        console.log("Conexion exitosa con MongoDB!")
+        console.log(`DATABASE: ${process.env.NODE_ENV === 'test' ? 'podcasthub_test' : 'podcasthub'}`);
+
+    } catch (error) {
+        //Si la contrasenia esta mal o la ip no esta autorizada entra aqui
+        console.error("Error de conexion con MongoDB", error.message)
+
+        //Matamos el proceso, porque sin base de datos la APi no sirve para nada
+        process.exit(1)
+    }
+
+};
+
+export default dbConnect;
