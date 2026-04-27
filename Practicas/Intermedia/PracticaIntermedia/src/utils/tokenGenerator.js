e98a39890be3db3cfc420273841ce475c471911c
XLLAMAA <jaimeadanez11@gmail.com>
2026-04-27 19:41:39 +0200

    Feat: He creado y hecho el codigo que se encarga de generar los tokens

---
+++ Practicas/Intermedia/PracticaIntermedia/src/utils/tokenGenerator.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import config from "../config/index.js";
+
+export const generateTokens = (userId) => {
+
+    //Token de acceso - 15 mins
+    const accesToken = jwt.sign(
+        { id: userId },
+        config.jwt.access.secret,
+        { expiresIn: config.jst.access.expiresIn }
+    )
+
+    //Token de refresco - 7 dias
+    const refreshToken = jwt.sign(
+        { id: userId },
+        config.jwt.refresh.secret,
+        { expiresIn: config.jwt.refresh.expiresIn }
+    )
+
+    return (
+        accesToken,
+        refreshToken
+    )
+
+
+}
