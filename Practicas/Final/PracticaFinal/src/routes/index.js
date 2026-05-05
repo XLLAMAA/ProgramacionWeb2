90be51dc1b4a6fa6c7511d9e80804290f0c63221
XLLAMAA <jaimeadanez11@gmail.com>
2026-05-05 10:07:51 +0200

    Feat: Rutas de client, project y delivery agregado al index

--- Practicas/Final/PracticaFinal/src/routes/index.js
+++ Practicas/Final/PracticaFinal/src/routes/index.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import userRoutes from "./user.routes.js";
+import clientRoutes from "./client.routes.js";
+import projectRoutes from "./project.routes.js";
+import deliveryRoutes from "./deliverynote.routes.js";
 
 const router = Router();
 
 //Ruta usuario
-router.use("/user", userRoutes);
+router.use("/user", userRoutes)
+//Ruta cliente
+router.use("/client", clientRoutes)
+//Ruta projject
+router.use("/project", projectRoutes)
+//Ruta delivery
+router.use("/delivery", deliveryRoutes)
 
 export default router;
